c3233405fd40c9ed40248bf1f893f7e1ed9c5361
JB184351 <justinbengtson@icloud.com>
2025-02-20 12:21:06 -0800

    Created initial project

---
+++ FetchTakeHome/FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Justin on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
